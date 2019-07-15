ba625ca8ae0140479585e14bbba2abdc51988651
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-07-15 11:10:35 +0700

    beware of my bug creation. kills you instantly

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int 
+main(int argv, char ** argc)
+{
+    // oh no, never ending loop
+    // this is a bug, not supposed to happen
+    // fix later PLZzZzZ
+    // Kappa
+    for(;;){
+	printf("nge bug ? 0.1 + 0.2 = %.20f        ", (0.1 + 0.2));
+	std::cout << "Haaaaaaaaaaaa ?" << std::endl;
+    }
+}
